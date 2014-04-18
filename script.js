fef91f2a11c98a5a33580fb128cc19d75b6e31d4
lucasw <lucasw_backwards@gmail.com>
2014-04-17 22:32:11 -0700

    Manhattan distance placeholder view function.

--- script.js
+++ script.js
@@ -21,28 +21,77 @@
 var grid;
 var SC = 16;
 
+var map_wd = 50;
+var map_ht = 30;
+
+var px;
+var py;
+
+function isInView(vx, vy, x, y) {
+
+  if (
+      (Math.abs(vx - x) < 10) &&
+      (Math.abs(vy - y) < 10)
+      )
+    return true;
+
+  return false;
+}
+
 function init() {
   
   stage = new createjs.Stage("Rogueview");
   stage.scaleX = SC;
   stage.scaleY = SC;
-  grid = new Array(50);
+
+  px = Math.floor(Math.random() * map_wd);
+  py = Math.floor(Math.random() * map_ht);
+
+  grid = new Array(map_wd);
   for (var i = 0; i < grid.length; i++) {
-    grid[i] = new Array(30);
+    grid[i] = new Array(map_ht);
+
     for (var j = 0; j < grid[i].length; j++) {
+      
+      var obstruction = 0;
+      if (Math.random() < 0.1)  obstruction = 1.0;
+      
+      grid[i][j] = obstruction;
+    }
+  }
+
+  for (var i = 0; i < grid.length; i++) {
+    for (var j = 0; j < grid[i].length; j++) {
+      var is_in_view = isInView(px, py, i, j);
+      //console.log(i + " " + j + " : " + grid[i][j]); 
       var tile = new createjs.Shape();
-      if (Math.random() > 0.1) {
-        tile.graphics.beginFill("#aaaaaa");
+      if (grid[i][j] <= 0.1) {
+        if (is_in_view) 
+          tile.graphics.beginFill("#aaaaaa");
+        else 
+          tile.graphics.beginFill("#777777");
       } else {
-        tile.graphics.beginFill("#666666");
+        if (is_in_view) 
+          tile.graphics.beginFill("#666666");
+        else 
+          tile.graphics.beginFill("#444444");
       }
       tile.graphics.drawRect(0, 0, 1, 1);
       tile.x = i;
       tile.y = j;
-      grid[i][j] = tile;
       stage.addChild(tile);
     }
   }
+  
+  { // character
+  var tile = new createjs.Shape();
+  tile.graphics.beginFill("#11cc22");
+  tile.graphics.drawRect(0, 0, 1, 1);
+  tile.x = px;
+  tile.y = py;
+  stage.addChild(tile);
+  }
+
   stage.update();
 }
 
